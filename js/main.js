2c71b1749b6516e69b15a5119de6a4373e8c0490
Thi Thu Hien Dinh <hien25042001@gmail.com>
2023-02-24 14:36:19 +0100

    up

--- js/main.js
+++ js/main.js
@@ -22,36 +22,36 @@ function dogName() {
 }
 
 // button Download
-// const downloadButton = document.getElementById('download-button');
-// const countdownTimer = document.getElementById('countdown-timer');
-// const downloadLink = document.getElementById('download-link');
+const downloadButton = document.getElementById('download-button');
+const countdownTimer = document.getElementById('countdown-timer');
+const downloadLink = document.getElementById('download-link');
 
-// let countdown;
-// let timeLeft = 10;
+let countdown;
+let timeLeft = 10;
 
-// function startCountdown() {
-//   downloadButton.disabled = true;
-//   timeLeft = 10;
-//   countdown = setInterval(() => {
-//     timeLeft--;
-//     if (timeLeft <= 0) {
-//       clearInterval(countdown);
-//       downloadButton.disabled = false;
-//       countdownTimer.textContent = '';
-//       timeLeft = 0;
-//       downloadButton.textContent = "Download";
-//       return;
-//     }
-//     countdownTimer.textContent = `Please wait ${timeLeft} seconds before downloading...`;
-//   }, 1000);
-// }
+function startCountdown() {
+  downloadButton.disabled = true;
+  timeLeft = 10;
+  countdown = setInterval(() => {
+    timeLeft--;
+    if (timeLeft <= 0) {
+      clearInterval(countdown);
+      downloadButton.disabled = false;
+      countdownTimer.textContent = '';
+      timeLeft = 0;
+      downloadButton.textContent = "Download";
+      return;
+    }
+    countdownTimer.textContent = `Please wait ${timeLeft} seconds before downloading...`;
+  }, 1000);
+}
 
-// downloadButton.addEventListener('click', () => {
-//   startCountdown();
-//   setTimeout(() => {
-//     downloadLink.click();
-//   }, timeLeft * 1000);
-// });
+downloadButton.addEventListener('click', () => {
+  startCountdown();
+  setTimeout(() => {
+    downloadLink.click();
+  }, timeLeft * 1000);
+});
 
 function toggleBlackout() {
   var elements = document.querySelectorAll('html *:not(button)');
