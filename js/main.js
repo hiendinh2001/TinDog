b3093075b02d7a2f02c2ba0bf810ace9af9cd3c1
Thi Thu Hien Dinh <hien25042001@gmail.com>
2023-02-24 07:19:40 +0100

    up

--- js/main.js
+++ js/main.js
@@ -3,19 +3,19 @@ const countdownTimer = document.getElementById('countdown-timer');
 const downloadLink = document.getElementById('download-link');
 
 let countdown;
-let timeLeft = 10; // in seconds
+let timeLeft = 10; 
 
 function startCountdown() {
-    downloadButton.disabled = true; // disable the download button
-    timeLeft = 10; // reset timeLeft to its initial value
+    downloadButton.disabled = true; 
+    timeLeft = 10; 
     countdown = setInterval(() => {
       timeLeft--;
       if (timeLeft <= 0) {
         clearInterval(countdown);
-        downloadButton.disabled = false; // enable the download button
+        downloadButton.disabled = false; 
         countdownTimer.textContent = '';
-        timeLeft = 0; // set timeLeft to zero to prevent negative values
-        downloadButton.textContent = "Download"; // reset download button text
+        timeLeft = 0; 
+        downloadButton.textContent = "Download"; 
         return;
       }
       countdownTimer.textContent = `Please wait ${timeLeft} seconds before downloading...`;
@@ -26,8 +26,6 @@ function startCountdown() {
 
 downloadButton.addEventListener('click', () => {
   startCountdown();
-
-  // create a link to the file and simulate a click on it
   setTimeout(() => {
     downloadLink.click();
   }, timeLeft * 1000);
